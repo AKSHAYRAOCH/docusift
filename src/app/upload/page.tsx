3d21ab51ffb57bd1ac8465b547e0c91c691b4fcc
ShreyaKandhagatla <shreyakandhagatla27@gmail.com>
2024-08-09 14:22:06 +0530

    upload multiple documents

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,22 +1,33 @@
 "use client";
- 
-import { UploadButton } from "@/utils/uploadthing";
- 
+
+import { UploadDropzone } from "@/utils/uploadthing";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <UploadButton
-        endpoint="imageUploader"
+    <main className="flex min-h-screen flex-col items-center justify-center p-6">
+      <UploadDropzone
+        endpoint="fileUploader"
+        accept="image/*,.pdf" // Ensure this attribute allows both images and PDFs
+        className="w-full max-w-lg h-64 border-2 border-dashed border-gray-400 flex flex-col items-center justify-center text-gray-600 hover:bg-gray-100 focus:ring-2 focus:ring-indigo-500"
         onClientUploadComplete={(res) => {
-          // Do something with the response
           console.log("Files: ", res);
           alert("Upload Completed");
         }}
         onUploadError={(error: Error) => {
-          // Do something with the error.
           alert(`ERROR! ${error.message}`);
         }}
       />
     </main>
   );
 }
+
+
+
+const BASE_URL = "https://utfs.io/f/"; 
+
+export async function generateFileUrls(fileKeys: string[]): Promise<string[]> {
+  const urls = fileKeys.map((key) => `${BASE_URL}${key}`);
+
+  return urls;
+}
+
